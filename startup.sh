51b165b31f7e47f71cc20b50c71f68bd402d3535
Dario Cazzani <dcazzani@cisco.com>
2016-05-11 14:58:06 +0200

    Added startup script

---
+++ startup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# activate environment variables
+source ~/.profile_Uno
+
+# change to bare repo
+cd /home/pi/Dev/Uno.git
+
+# set environment variable to version
+export UNO_VERSION="$(git rev-parse HEAD | cut -c 1-10)"
+
+# install requirements
+cd /home/pi/Dev/Uno
+
+pip install --user -r requirements.txt
+
+# cd out of repo
+cd ~/
+
+# send IP to admin
+
+# start services
+cd /home/pi/Dev
+python start.Ãpy
